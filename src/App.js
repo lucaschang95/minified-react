fb17585eb0bf6fd90e4d42ac8d57165f32d46e8c
changxingzhao <changxingzhao@kuaishou.com>
2021-11-24 20:38:44 +0800

    feat: debug react

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React from 'react';
+
 class App extends React.Component {
-    componentDidMount() {
-        console.log(`App Mount`);
-        console.log(`App 组件对应的fiber节点: `, this._reactInternals);
-    }
-    render() {
-        return (
-        <div className="app">
-            <header>header</header>
-            <Content />
-        </div>
-        );
-    }
+  componentDidMount() {
+    console.log(`App Mount`);
+    console.log(`App 组件对应的fiber节点: `, this._reactInternals);
+  }
+  render() {
+    return (
+      <div className="app">
+        <header>header</header>
+        <Content />
+      </div>
+    );
+  }
 }
 
 class Content extends React.Component {
-    componentDidMount() {
-        console.log(`Content Mount`);
-        console.log(`Content 组件对应的fiber节点: `, this._reactInternals);
-    }
-    render() {
-        return (
-        <React.Fragment>
-            <p>1</p>
-            <p>2</p>
-        </React.Fragment>
-        );
+  componentDidMount() {
+    console.log(`Content Mount`);
+    console.log(`Content 组件对应的fiber节点: `, this._reactInternals);
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <p>1</p>
+        <p>2</p>
+      </React.Fragment>
+    );
     }
 }
 export default App;
