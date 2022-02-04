a3c29b623e042b3b6613dec8e9530606c834f99c
changxingzhao <changxingzhao@kuaishou.com>
2022-02-04 22:34:34 +0800

    feat(router): add router component

--- src/App.js
+++ src/App.js
@@ -6,26 +6,11 @@ export default function App() {
         <BrowserRouter>
             <div>
                 <h1>Basic Example</h1>
-                <p>
-                    This example demonstrates some of the core features of React
-                    Router including nested <code>&lt;Route&gt;</code>s,{' '}
-                    <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s,
-                    and using a "*" route (aka "splat route") to render a "not
-                    found" page when someone visits an unrecognized URL.
-                </p>
-
-                {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
                 <Routes>
                     <Route path="/" element={<Layout />}>
                         <Route index element={<Home />} />
                         <Route path="about" element={<About />} />
                         <Route path="dashboard" element={<Dashboard />} />
-
-                        {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
                         <Route path="*" element={<NoMatch />} />
                     </Route>
                 </Routes>
@@ -57,6 +42,8 @@ function Layout() {
             </nav>
 
             <hr />
+            <div>next is outlet</div>
+            <Outlet />
             <Outlet />
         </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
         port: 3000,
         hot: true,
         open: true,
+        historyApiFallback: true,
     },
     plugins: [new webpack.HotModuleReplacementPlugin()],
 };
